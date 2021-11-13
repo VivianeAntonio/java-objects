c021740f9b3a9a273ed4f51cdff8706c0dfca0ef
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-13 07:17:48 -0300

    Initial commit

---
+++ peixe.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class peixe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class peixe extends Actor
+{
+    /**
+     * Act - do whatever the peixe wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move (2);
+        turn (1);
+        move (1);
+    }    
+}
